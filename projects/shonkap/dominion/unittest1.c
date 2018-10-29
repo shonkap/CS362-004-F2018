6e006b4af5f606ca9343d77b57ee4f6c77d40824
shonkap <shonkap@oregonstate.edu>
2018-10-28 21:04:03 -0700

    added the other 4

--- projects/shonkap/dominion/unittest1.c
+++ projects/shonkap/dominion/unittest1.c
@@ -10,14 +10,52 @@
 
 //update coins
 int main(){
-    int r;
 
   int k[10] = {adventurer, council_room, feast, gardens, mine,
 	       remodel, smithy, village, baron, great_hall};
 
   struct gameState G;
   initializeGame(4, k, 1, &G);
-  for(int i = 0;i<4;i++){
-      
-  } 
+  G.coins = 0;
+   G.hand[0][0] = gold;
+   G.hand[0][1] = gold;
+   G.hand[0][2] = gold;
+   G.hand[0][3] = gold;
+   updateCoins(0,&G,0);
+
+   int t1 = assert(G.coins == 12);
+   G.coins = 0;
+
+   G.hand[0][0] = silver;
+   G.hand[0][1] = silver;
+   G.hand[0][2] = silver;
+   G.hand[0][3] = silver;
+   updateCoins(0,&G,0);
+
+   int t2 = assert(G.coins == 8);
+   G.coins = 0;
+
+   G.hand[0][0] = copper;
+   G.hand[0][1] = copper;
+   G.hand[0][2] = copper;
+   G.hand[0][3] = copper;
+   updateCoins(0,&G,0);
+
+   int t3 = assert(G.coins == 4);
+
+   G.hand[0][0] = smithy;
+   G.hand[0][1] = smithy;
+   G.hand[0][2] = smithy;
+   G.hand[0][3] = smithy;
+   updateCoins(0,&G,0);
+
+   int t4 = assert(G.coins == 4);
+
+   if(t1 + t2 + t3 + t4 == 0)
+        printf("test completed successfully! \n");
+    else
+        printf("test Failed! \n");
+
+
+
 }
--- projects/shonkap/dominion/unittest3.c
+++ projects/shonkap/dominion/unittest3.c
@@ -7,3 +7,26 @@
 
 #define DEBUG 0
 #define NOISY_TEST 1
+
+//whose turn function
+int main(){
+
+  int k[10] = {adventurer, council_room, feast, gardens, mine,
+	       remodel, smithy, village, baron, great_hall};
+
+  struct gameState G;
+  initializeGame(4, k, 1, &G);
+
+  G.whoseTurn = 0;
+
+  int t1 = assert(whoseTurn(G) == 0);
+
+  G.whoseTurn = 1;
+
+  int t2 = assert(whoseTurn(G) == 1);
+
+  if(t1 + t2 == 0)
+        printf("test completed successfully! \n");
+    else
+        printf("test Failed! \n");
+}
